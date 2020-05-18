17b87821cc93b0ab28657bc0016750e4e2f1d849
Abhishek Sharma <sharmaabhi91011@gmail.com>
2020-05-18 07:40:36 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the value of a and b:");
+    scanf("%d %d",&a,&b);
+    printf("the sum of a and b is %d\n",a+b );
+    printf("the differnce of a and b is %d\n",a-b );
+    printf("the product of a and b is %d\n",a*b);
+    printf("the quotient of a and b is %f\n",a/b );
+    return 0;
+}
