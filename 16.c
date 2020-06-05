c57d1979d9eb652259e346281d94838b85406044
Abhishek Sharma <sharmaabhi91011@gmail.com>
2020-06-05 16:31:28 +0530

    Add files via upload

---
+++ 16.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter the character to know its ASCII value:");
+    scanf("%c",&a);
+    printf("The ASCII value of %c Character is :%d ",a,a);
+    return 0;
+}
